cd0f54afb7fcd5e64be621f2d0fc585fecb89410
harshithb3304 <harsh03032004@gmail.com>
2024-06-13 16:53:25 +0530

    fixed gitignore file to ignore env

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,6 +1,6 @@
 require("dotenv").config()
 import OpenAI,{toFile} from "openai";
-console.log(process.env.OPENAI_API_KEY)
+
 export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || "",
 })
