d3a379d464d5e1c1b82c8eace3e5005c7a829ced
Harshith B <97750946+harshithb3304@users.noreply.github.com>
2024-07-01 14:39:18 +0530

    Update route.ts

--- src/app/api/add-project/upload/route.ts
+++ src/app/api/add-project/upload/route.ts
@@ -3,6 +3,7 @@ import { openai } from "@/lib/openai";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { NextRequest, NextResponse } from "next/server";
 
+export const maxDuration = 60;
 export const runtime = 'nodejs';
 
 // export const config = {
